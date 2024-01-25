1ff9b909d9737abb30e822798afb80cd5fdb8562
Chiptune93 <eoen012@gmail.com>
2024-01-25 14:53:48 +0900

    :rocket: project init

---
+++ src/test/kotlin/kopring/project/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package kopring.project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
